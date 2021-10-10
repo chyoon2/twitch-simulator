452a98681442c01dc6161b9339fe82ad3beea77a
Chris Yoon <chy.yoon@gmail.com>
2021-10-09 18:54:10 -0700

    add logic to incorporate google oath to comonenet

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,18 +1,35 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "982107182769-bpjvj896vn60jtuubbjiu161a62mfe1v.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "982107182769-bpjvj896vn60jtuubbjiu161a62mfe1v.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          //.init ^ sets off a promise.
+          this.auth = window.gapi.auth2.getAuthInstance();
+          //get a ref and save it to an instance of this class.
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
-
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we're signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed In! </div>;
+    } else {
+      return <div> I am not signed in</div>;
+    }
+  }
   render() {
-    return <div> Google Auth</div>;
+    return <div> {this.renderAuthButton()} </div>;
   }
 }
 
