fd29b091e7048f0752fc6324f04d925816955c5d
Chris Yoon <chy.yoon@gmail.com>
2021-10-10 22:39:39 -0700

    added redux form and validation to the create componenet

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,22 +2,57 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput(formProps) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className='ui error message'>
+          <div className='header'>{error}</div>
+        </div>
+      );
+    }
+  }
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+
     return (
-      <input
-        {...formProps.input}
-        // onChange={formProps.input.onChange}
-        // value={formProps.input.value}>
-      />
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete='off' />
+        <div>{this.renderError(meta)}</div>
+      </div>
     );
+  };
+  onSubmit(formValues) {
+    console.log(formValues);
   }
   render() {
     return (
-      <form>
-        <Field name='title' component={this.renderInput} />
-        <Field name='description' component={this.renderInput} />
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className='ui form error'>
+        <Field name='title' component={this.renderInput} label='Enter Title' />
+        <Field
+          name='description'
+          component={this.renderInput}
+          label='Enter Description'
+        />
+        <button className='ui button primary'>Submit</button>
       </form>
     );
   }
 }
-export default reduxForm({ form: "streamCreate" })(StreamCreate);
+
+const validate = (formValues) => {
+  //formValues has all the values from the form
+  const errors = {};
+
+  if (!formValues.title) {
+    //only if user entered NO title, in which case must return an object that has for each invalid field, put a key-value pair on the object with the name of the field and the error message. errors={title: 'you must enter a title'}
+    errors.title = "You must enter a Title";
+  }
+  if (!formValues.description) {
+    errors.description = "You must enter a Description";
+  }
+  return errors;
+};
+export default reduxForm({ form: "streamCreate", validate })(StreamCreate);
