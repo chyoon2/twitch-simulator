c200a1e838f9943aa473f689f2cfe1a5d5f2ca87
Chris Yoon <chy.yoon@gmail.com>
2021-10-12 13:44:06 -0700

    add a action and type that posts to json server

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from "../apis/streams";
-import { SIGN_OUT, SIGN_IN } from "./types";
+import { SIGN_OUT, SIGN_IN, CREATE_STREAM } from "./types";
 
 export const signIn = (userId) => {
   return {
@@ -15,5 +15,6 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues) => async (dispatch) => {
-  streams.post("/streams", formValues);
+  const response = await streams.post("/streams", formValues);
+  dispatch({ type: CREATE_STREAM, payload: response.data });
 };
