8a52d243e193f391287937cbfb513a98d19ff3c7
Chris Yoon <chy.yoon@gmail.com>
2021-10-13 14:03:37 -0700

    refactor route to the edit and add nav to edit in streamlist

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const StreamEdit = () => {
-  return <div>Stream e</div>;
+  return <div>Stream edit</div>;
 };
 
 export default StreamEdit;
+
+// selectionReducer when a user clicks on a stream to edit isFinite, use this reducer to record what stream is being edited.
+//use router. goes to a specific address of the stream they are trying to edit. put the id of the stream in the url .
+//
