9d402ab20f7b2652b4d62a38104cc97eb774b941
Chris Yoon <chy.yoon@gmail.com>
2021-10-13 16:17:04 -0700

    refactor edit component to take submit and use form

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,23 +1,44 @@
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions/index";
+import { editStream, fetchStream } from "../../actions/index";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = (formValues) => {
+    console.log(formValues);
+    console.log(this.props.match.params.id);
+
+    this.props.editStream(this.props.match.params.id, formValues);
+  }; //this will recieve form values from the user, and call parent fxn
+
   render() {
     if (!this.props.stream) {
       return <div>loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3> Edit Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, "title", "description")}
+          //lodash to pick out things from state, to pass on.
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
-
+//initialValues is a special redux form name
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
 
 // selectionReducer when a user clicks on a stream to edit isFinite, use this reducer to record what stream is being edited.
 //use router. goes to a specific address of the stream they are trying to edit. put the id of the stream in the url .
