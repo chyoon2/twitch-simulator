7144b1128ef663ec2074ebe6aa75cef0af7ac7d7
Chris Yoon <chy.yoon@gmail.com>
2021-10-13 13:12:09 -0700

    added buttons admin fxns and navigation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -23,9 +23,14 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues) => async (dispatch, getState) => {
+  // below: passing userId in order to have the id in the server, so that we can use it to show admin fxns to the user if they are signed in.
   const { userId } = getState().auth;
   const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  //do some programmatic navigation to get the user back to the root route
+  //sometimes its hard, sometimes its easy.
+  //have to use the history obj in the browser, that BrowserRouter uses. so instead make your own browser history and dont use browserrouter.
+  //remove browser router in your app.js/action-indexjs
 };
 
 export const fetchStream = (id) => async (dispatch) => {
