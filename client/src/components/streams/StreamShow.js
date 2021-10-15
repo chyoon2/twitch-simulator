7312189f8a73c35b7d58513066b6ba988a2c5c05
Chris Yoon <chy.yoon@gmail.com>
2021-10-15 15:29:21 -0700

    added LIVE  streaming fxnality to streamshow componenet

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { flv } from "flv.js";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
@@ -9,7 +9,30 @@ class StreamShow extends React.Component {
     this.videoRef = React.createRef();
   }
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
   render() {
     if (!this.props.stream) {
