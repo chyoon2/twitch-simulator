6b3ab60cd90410c7adce0fb39088db5dc18b20d4
Chris Yoon <chy.yoon@gmail.com>
2021-10-14 23:11:36 -0700

    adding routing of delete buttons from the app to lead to modal

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,23 +1,33 @@
 import React from "react";
 import Modal from "../Modal";
+import history from "../../history";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className='ui button negative'>Delete</button>
+        <button className='ui button'>Cancel</button>
+      </React.Fragment>
+    );
+  }
+  render() {
+    return (
+      <div>
+        Stream delete
+        <Modal
+          title='Delete Stream'
+          content='Are you sure you want to delete this stream'
+          actions={this.renderActions()}
+          onDismiss={() => history.push("/")}
+        />
+      </div>
+    );
+  }
+}
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <button className='ui button negative'>Delete</button>
-      <button className='ui button'>cancel</button>
-    </React.Fragment>
-  );
-  return (
-    <div>
-      Stream delete
-      <Modal
-        title='Delete Stream'
-        content='Are you sure you want to delete this stream'
-        actions={actions}
-      />
-    </div>
-  );
-};
-
-export default StreamDelete;
+export default connect(null, { fetchStream })(StreamDelete);
