4fa8e4e6ac43365096354f591efe91ed8a7bf783
Chris Yoon <chy.yoon@gmail.com>
2021-10-14 22:37:28 -0700

    added js modal designs and buttons, onclick fxns

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import history from "../history";
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className='ui dimmer modals visibile active'>
-      <div className='ui standard modal visible active'>a;lsdfjasl;dkfj</div>
+    <div
+      onClick={() => history.push("/")}
+      className='ui dimmer modals visibile active'>
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className='ui standard modal visible active'>
+        <div className='header'>Delete Stream</div>
+        <div className='content'>
+          Are you sure you want to Delete this stream?
+        </div>
+        <div className='actions'>
+          <div className='ui button primary'>Delete</div>
+          <div className='ui button'>Cancel</div>
+        </div>
+      </div>
     </div>,
     document.querySelector("#modal")
   );
